85c2e7175965d25b2a8eb0fb09d485e15c471a41
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-09-16 20:38:42 +1000

    Defined #authorize_url and #authorize methods. Return OAuth2::Client from #initialize

--- lib/gplus/client.rb
+++ lib/gplus/client.rb
@@ -3,6 +3,23 @@ module Gplus
     def initialize(options = {})
       @client_id = options[:client_id]
       @client_secret = options[:client_secret]
+      @redirect_uri = options[:redirect_uri]
+
+      @oauth_client = OAuth2::Client.new(
+        @client_id,
+        @client_secret,
+        :site => 'https://www.googleapis.com/plus/',
+        :authorize_url => 'https://accounts.google.com/o/oauth2/auth',
+        :token_url => 'https://accounts.google.com/o/oauth2/token'
+      )
+    end
+
+    def authorize_url(redirect_uri = @redirect_uri)
+      @oauth_client.auth_code.authorize_url(:redirect_uri => redirect_uri, :scope => 'https://www.googleapis.com/auth/plus.me')
+    end
+
+    def authorize(auth_code, redirect_uri = @redirect_uri)
+      @access_token = @oauth_client.auth_code.get_token(auth_code, :redirect_uri => redirect_uri)
     end
   end
 end
