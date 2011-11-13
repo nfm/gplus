b7d515a2931bb7eda7f3a728176ba41b74c4e745
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-11-13 14:45:43 +1100

    Tidy up person_spec

--- spec/gplus/person_spec.rb
+++ spec/gplus/person_spec.rb
@@ -4,13 +4,14 @@ describe Gplus::Client do
   before do
     @api_key = '1234567'
     @client = Gplus::Client.new(:api_key => @api_key)
+
+    @person, @person_json = fixture('person.json')
   end
 
   describe '.get_person' do
     it "should return a person's profile" do
-      person, person_json = fixture('person.json')
-      stub_api_request(:get, "people/#{person_json['id']}").to_return(:body => person)
-      @client.get_person(person_json['id']).should == person_json
+      stub_api_request(:get, "people/#{@person_json['id']}").to_return(:body => @person)
+      @client.get_person(@person_json['id']).should == @person_json
     end
   end
 end
