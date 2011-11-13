8a0791aa7d336271c69f6abc72ab3fa34d184862
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-11-13 12:45:26 +1100

    Write example for #get_person

--- spec/gplus/person_spec.rb
+++ spec/gplus/person_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
 describe Gplus::Client do
+  before do
+    @api_key = '1234567'
+    @client = Gplus::Client.new(:api_key => @api_key)
+  end
+
   describe '.get_person' do
-    pending
+    it "should return a person's profile" do
+      person, person_json = fixture('person.json')
+      stub_api_request(:get, "people/#{person_json['id']}").to_return(:body => person)
+      @client.get_person(person_json['id']).should == person_json
+    end
   end
 end
