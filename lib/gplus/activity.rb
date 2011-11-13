1f85e0a8548cf36bce54f161ea7975af5c01bf67
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-11-13 14:32:49 +1100

    Tidy up documentation for Activity, Comment and Person

--- lib/gplus/activity.rb
+++ lib/gplus/activity.rb
@@ -4,7 +4,7 @@ module Gplus
     # See http://developers.google.com/+/api/latest/activities/get for more details.
     #
     # @param [String] id The unique ID of the activity you want to retrieve.
-    # @return [Hash] A nested hash representation of an {http://developers.google.com/+/api/latest/activities Activity resource}.
+    # @return [Hash] A nested hash representation of an {http://developers.google.com/+/api/latest/activities#resource Activity resource}.
     def get_activity(id)
       get("activities/#{id}")
     end
@@ -13,9 +13,9 @@ module Gplus
     # See http://developers.google.com/+/api/latest/activities/list for more details.
     #
     # @param [String] person_id The unique ID of the person whose activities you want to list. Pass the string 'me' to list the activities for the person that the API client is authorized as.
-    # @option options [Integer] maxResults The number of activities, between 1 and 100, to return.
+    # @option options [Integer] maxResults The number of activities, between 1 and 100, to return. Defaults to 20.
     # @option options [String] pageToken The page of activities to fetch. Pass the value of :nextPageToken from the previous result set to get the next page of results.
-    # @return [Hash] A nested hash representation of {http://developers.google.com/+/api/latest/activities/list list of activities}.
+    # @return [Hash] A nested hash representation of a {http://developers.google.com/+/api/latest/activities/list#response list of activities}.
     def list_activities(person_id, options = {})
       get("people/#{person_id}/activities/public", options)
     end
--- lib/gplus/person.rb
+++ lib/gplus/person.rb
@@ -4,7 +4,7 @@ module Gplus
     # See http://developers.google.com/+/api/latest/people/get for more details.
     #
     # @param [String] id The unique ID of the person whose profile you want to retrieve. Pass the string 'me' to fetch the profile for the person that the API client is authorized as.
-    # @return [Hash] A nested hash representation of a {http://developers.google.com/+/api/latest/people/get Person resource}
+    # @return [Hash] A nested hash representation of a {http://developers.google.com/+/api/latest/people#resource Person resource}
     def get_person(id)
       get("people/#{id}")
     end
