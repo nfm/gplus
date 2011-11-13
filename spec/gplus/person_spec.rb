20306d6d785bb840227803b097a25fe1ef938eb5
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-11-13 14:46:53 +1100

    Define #search_people and write examples for it

--- spec/gplus/person_spec.rb
+++ spec/gplus/person_spec.rb
@@ -14,4 +14,34 @@ describe Gplus::Client do
       @client.get_person(@person_json['id']).should == @person_json
     end
   end
+
+  describe '.search_people' do
+    before do
+      @people, @people_json = fixture('people.json')
+    end
+
+    it "should list all public profiles" do
+      stub_api_request(:get, "people").to_return(:body => @people)
+      @client.search_people.should == @people_json
+    end
+
+    it "should accept a :query argument" do
+      stub_api_request(:get, "people", :query => @person_json['displayName']).to_return(:body => @person)
+      @client.search_people(:query => @person_json['displayName']).should == @person_json
+    end
+
+    it "should accept a :maxResults argument" do
+      @results = 2
+
+      stub_api_request(:get, "people", :maxResults => @results.to_s).to_return(:body => @people)
+      @client.search_people(:maxResults => @results).should == @people_json
+    end
+
+    it "should accept a :pageToken argument" do
+      @page = '1234567'
+
+      stub_api_request(:get, "people", :pageToken => @page).to_return(:body => @people)
+      @client.search_people(:pageToken => @page).should == @people_json
+    end
+  end
 end
