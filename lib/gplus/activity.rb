e9f4fe7e407490f1a6d385e1ef02a87b7ef5c344
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2011-12-30 19:16:56 +1100

    Tidy up documentation in activity.rb

--- lib/gplus/activity.rb
+++ lib/gplus/activity.rb
@@ -13,7 +13,7 @@ module Gplus
     # See http://developers.google.com/+/api/latest/activities/list for more details.
     #
     # @param [String] person_id The unique ID of the person whose activities you want to list. Pass the string 'me' to list the activities for the person that the API client is authorized as.
-    # @option options [Integer] maxResults The number of activities, between 1 and 100, to return. Defaults to 20.
+    # @option options [Integer] maxResults (20) The number of activities, between 1 and 100, to return.
     # @option options [String] pageToken The page of activities to fetch. Pass the value of :nextPageToken from the previous result set to get the next page of results.
     # @return [Hash] A nested hash representation of a {http://developers.google.com/+/api/latest/activities/list#response list of activities}.
     def list_activities(person_id, options = {})
@@ -23,10 +23,10 @@ module Gplus
     # Search all public Google+ activities.
     # See https://developers.google.com/+/api/latest/activities/search for more details
     #
-    # @option options [String] query The full text query to search for
-    # @option options [Integer] maxResults The number of activities, between 1 and 20, to return. Defaults to 10.
+    # @option options [String] query The full text query to search for. This option is required.
+    # @option options [Integer] maxResults (10) The number of activities, between 1 and 20, to return.
     # @option options [String] pageToken The page of activities to fetch. Pass the value of :nextPageToken from the previous result set to get the next page of results.
-    # @option options [String] orderBy Specifies how to order search results. Acceptable values are "best" and "recent". Defaults to "recent".
+    # @option options [String] orderBy ('recent') Specifies how to order search results. Acceptable values are 'best' and 'recent'.
     # @return [Hash] A nested hash representation of a {http://developers.google.com/+/api/latest/activities/search#response search result for activities}.
     def search_activities(options = {})
       get("activities", options)
